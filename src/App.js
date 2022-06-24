7e0bd2d0b04af41b17549f6e2ba48afd60896db4
Sohag <sohag15-9065@diu.edu.bd>
2022-06-24 16:26:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Laptops from './components/LaptopCorner/LaptopCorner';
+import Header from './components/Header/Header';
+import LaptopCorner from './components/LaptopCorner/LaptopCorner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <LaptopCorner></LaptopCorner>
     </div>
   );
 }
